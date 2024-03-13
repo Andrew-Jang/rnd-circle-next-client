a865815f7382e9cd44e5b74b779bb6348cc1be77
동해 <andrew@donghaeui-Macmini.local>
2024-03-13 13:18:25 +0900

    .

--- app/(routes)/test2/page.js
+++ app/(routes)/test2/page.js
@@ -131,29 +131,153 @@ export default function Test2() {
     </div>
   );
 
+  const Bar = ({ title, height, color }) => (
+    <div className="flex flex-col items-center">
+      <div className="relative">
+        <Image src="/bar.png" alt="Vercel Logo" className="" width={50} height={150} />
+        <div
+          style={{ borderRadius: "15px", backgroundColor: color, width: 50, height }}
+          className="absolute bottom-0 w-full"
+        ></div>
+      </div>
+      <p style={{ fontSize: "10px", color: "#525252" }} className="font-montserrat mt-1">
+        {title}
+      </p>
+    </div>
+  );
   const CardSection = () => (
-    <div style={{ marginTop: "-245px" }} className="w-full bg-opacity-50 px-16 z-20 flex">
-      <div className="w-1/2 h-full"></div>
-      <div className="w-1/2 h-full p-8 -ml-4 flex space-x-8">
-        <button
-          style={{
-            backdropFilter: "blur(10px)",
-            backgroundColor: "rgba(255, 255, 255, 0.5)",
-            borderColor: "#ECECEC",
-            borderRadius: "26.39px",
-            height: "212px",
-          }}
-          className="w-48 h-24 rounded-xl border-2 border-white flex flex-col justify-between p-8 py-8 transition flex-shrink-0"
-        >
-          <p style={{ fontWeight: "500", fontSize: "27px" }} className="font-montserrat text-2xl text-left leading-7">
-            Edit <br /> Website
-          </p>
-          <p style={{ color: "#757575" }} className="text-xs text-left">
-            나의 웹사이트에 <br /> 새 정보를 등록해보세요
-          </p>
-        </button>
+    <div style={{ marginTop: "-245px" }} className="w-full h-full bg-opacity-50 px-16 z-20 flex">
+      <div className="w-full h-full">
+        <div style={{ height: "246px" }} className="w-full bg-opacity-50 flex justify-end pt-8 pr-8">
+          <button
+            style={{
+              backdropFilter: "blur(10px)",
+              backgroundColor: "rgba(255, 255, 255, 0.5)",
+              borderColor: "#ECECEC",
+              borderRadius: "26.39px",
+              // height: "212px",
+            }}
+            className="w-48 h-full rounded-xl border-2 border-white flex flex-col justify-between p-8 py-8 transition flex-shrink-0"
+          >
+            <p style={{ fontWeight: "500", fontSize: "27px" }} className="font-montserrat text-2xl text-left leading-7">
+              Edit <br /> Website
+            </p>
+            <p style={{ color: "#757575" }} className="text-xs text-left">
+              나의 웹사이트에 <br /> 새 정보를 등록해보세요
+            </p>
+          </button>
+        </div>
+        <div style={{ height: "548px" }} className="w-full h-96 bg-opacity-50 pt-8 pr-8 flex space-x-8">
+          <button
+            style={{
+              backdropFilter: "blur(10px)",
+              backgroundColor: "rgba(255, 255, 255, 0.5)",
+              borderColor: "#ECECEC",
+              borderRadius: "26.39px",
+              // height: "212px",
+            }}
+            className="w-72 h-full rounded-xl border-2 border-white flex flex-col justify-between p-8 py-8 transition flex-shrink-0"
+          >
+            <div className="space-y-6 flex flex-col items-start">
+              <p
+                style={{ fontWeight: "500", fontSize: "27px" }}
+                className="font-montserrat text-2xl text-left leading-7"
+              >
+                Article
+              </p>
+              <p style={{ color: "#757575" }} className="text-xs text-left">
+                AI와 함께 나의 연구실 웹사이트에
+                <br /> 게시할 새로운 아티클을 생성해보세요
+              </p>
+              <button className="bg-black rounded-full py-1.5 px-4 text-white font-montserrat text-xs font-medium">
+                Create
+              </button>
+            </div>
+            <div className="mt-8">
+              <Image src="/article.png" alt="Vercel Logo" className="" width={290} height={370} />
+            </div>
+          </button>
+          <div className="space-y-8 w-full">
+            <button
+              style={{
+                backdropFilter: "blur(10px)",
+                backgroundColor: "rgba(255, 255, 255, 0.5)",
+                borderColor: "#ECECEC",
+                borderRadius: "26.39px",
+                height: "17.2rem",
+              }}
+              className="w-full rounded-xl border-2 border-white flex flex-col justify-between px-3 pt-8 pb-3 transition flex-shrink-0"
+            >
+              <div className="flex items-center justify-between w-full px-5 mb-8">
+                <p style={{ fontWeight: "500", fontSize: "27px" }} className="font-montserrat text-2xl text-left">
+                  Visitor metrics
+                </p>
+                <div className="flex items-center space-x-2">
+                  <button
+                    style={{ backgroundColor: "#000AFF" }}
+                    className="rounded-full py-1.5 px-4 text-white font-montserrat text-xs font-medium"
+                  >
+                    웹조회수
+                  </button>
+                  <button
+                    style={{ backgroundColor: "#FBFBFB", borderColor: "#ECECEC", color: "#757575" }}
+                    className="bg-black rounded-full py-1.5 px-4 text-white font-montserrat text-xs font-medium border"
+                  >
+                    매칭 제안수
+                  </button>
+                </div>
+              </div>
+              <div
+                style={{ borderColor: "#ECECEC", borderRadius: "25px" }}
+                className="w-full h-64 border-2 relative flex items-center px-5 justify-between pt-2"
+              >
+                <Bar title={"MON"} height={"4rem"} color={"#9397FF"} />
+                <Bar title={"TUE"} height={"2rem"} color={"#C7CAFF"} />
+                <Bar title={"WED"} height={"3rem"} color={"#6F75FF"} />
+                <Bar title={"THU"} height={"4rem"} color={"#323AFF"} />
+                <Bar title={"MON"} height={"3rem"} color={"#6F75FF"} />
+                <Bar title={"MON"} height={"5rem"} color={"#000AFF"} />
+                <Bar title={"MON"} height={"7.7rem"} color={"#000000"} />
+              </div>
+            </button>
+            <button
+              style={{
+                backdropFilter: "blur(10px)",
+                backgroundColor: "rgba(255, 255, 255, 0.5)",
+                borderColor: "#ECECEC",
+                borderRadius: "26.39px",
+                height: "13rem",
+              }}
+              className="w-full h-52 rounded-xl border-2 border-white flex flex-col justify-between py-8 px-3 transition flex-shrink-0"
+            >
+              <div className="space-y-8 flex flex-col items-start">
+                <p
+                  style={{ fontWeight: "500", fontSize: "27px" }}
+                  className="font-montserrat text-2xl text-left leading-7 px-5"
+                >
+                  Visitor metrics
+                </p>
+                <div
+                  style={{ borderColor: "#ECECEC", borderRadius: "25px", height: "6.5rem" }}
+                  className="w-full h-24 border-2 relative"
+                >
+                  <Image src="/metrics.png" alt="Vercel Logo" className="" width={537} height={120} />
+                  <Image
+                    src="/metrics-graph.png"
+                    alt="Vercel Logo"
+                    className="absolute bottom-0"
+                    width={637}
+                    height={110}
+                  />
+                </div>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
 
-        <div className="flex-shrink-0 space-y-8">
+      <div className="w-auto h-full py-8 flex space-x-8 flex-shrink-0">
+        <div className="flex-shrink-0 space-y-6">
           <div className="relative">
             <Image src="/union.png" alt="Vercel Logo" className="" width={400} height={151} />
             <div className="w-full h-full absolute top-0 p-8 py-8">
@@ -190,17 +314,67 @@ export default function Test2() {
           </div>
           <div className="relative">
             <Image src="/union-big.png" alt="Vercel Logo" className="" width={400} height={151} />
-            <div className="w-full h-full absolute top-0 p-8 py-8">
-              <p
-                style={{ fontWeight: "500", fontSize: "27px" }}
-                className="font-montserrat text-2xl text-left leading-7"
-              >
-                새롭게 등록된 <br /> 프로젝트를 확인해보세요
-              </p>
-              <p style={{ color: "#757575" }} className="text-xs text-left mt-3">
-                72개의 새로운 R&D 프로젝트 공고가 업데이트되었어요
-              </p>
+            <div className="w-full h-full absolute top-0 p-8 py-8 flex flex-col justify-between">
+              <div>
+                <p
+                  style={{ fontWeight: "500", fontSize: "27px" }}
+                  className="font-montserrat text-2xl text-left leading-7"
+                >
+                  새롭게 등록된 <br /> 프로젝트를 확인해보세요
+                </p>
+                <p style={{ color: "#757575" }} className="text-xs text-left mt-3">
+                  72개의 새로운 R&D 프로젝트 공고가 업데이트되었어요
+                </p>
+              </div>
+
+              <div className="space-y-5">
+                <button
+                  style={{
+                    backdropFilter: "blur(10px)",
+                    backgroundColor: "rgba(255, 255, 255, 0.5)",
+                    borderColor: "#ECECEC",
+                    borderRadius: "26.39px",
+                  }}
+                  className="w-full h-24 rounded-xl border-2 border-white flex items-center px-4 transition flex-shrink-0 space-x-4"
+                >
+                  <div
+                    style={{ backgroundColor: "#000AFF" }}
+                    className="w-11 h-11 rounded-full flex-shrink-0 flex items-center justify-center"
+                  >
+                    <Image src="/bank.png" alt="Vercel Logo" className="" width={18} height={18} />
+                  </div>
+                  <div className="w-full text-left">
+                    <p className="font-montserrat font-medium">Quantum Innovation Lab</p>
+                    <p style={{ color: "#757575", fontSize: "10px" }} className="mt-0.5">
+                      서울과학기술대학교 양자기술 글로벌 혁신 기술 연구실
+                    </p>
+                  </div>
+                </button>
+                <button
+                  style={{
+                    backdropFilter: "blur(10px)",
+                    backgroundColor: "rgba(255, 255, 255, 0.5)",
+                    borderColor: "#ECECEC",
+                    borderRadius: "26.39px",
+                  }}
+                  className="w-full h-24 rounded-xl border-2 border-white flex items-center px-4 transition flex-shrink-0 space-x-4"
+                >
+                  <div
+                    style={{ backgroundColor: "#000AFF" }}
+                    className="w-11 h-11 rounded-full flex-shrink-0 flex items-center justify-center"
+                  >
+                    <Image src="/bank.png" alt="Vercel Logo" className="" width={18} height={18} />
+                  </div>
+                  <div className="w-full text-left">
+                    <p className="font-montserrat font-medium">2024 GNP</p>
+                    <p style={{ color: "#757575", fontSize: "10px" }} className="mt-0.5">
+                      2024년도 차세대 원자력 정책센터 신규과제 공고
+                    </p>
+                  </div>
+                </button>
+              </div>
             </div>
+
             <button className="absolute top-0 right-0 hover:opacity-80">
               <Image src="/union-action.png" alt="Vercel Logo" width={54} height={54} />
             </button>
